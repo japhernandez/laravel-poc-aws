a941b5fce742fcff90029f7a312f07a9017aba47
John Piedrahita <citymanager02@gmail.com>
2023-01-07 15:18:18 +0100

    refactor

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -3,16 +3,16 @@ resource "aws_security_group" "alb" {
   vpc_id = aws_vpc.vpc.id
 
   ingress {
-    from_port   = 0
-    to_port     = 65535
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
     from_port   = 0
-    to_port     = 65535
-    protocol    = "tcp"
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
--- terraform/backend.tf
+++ terraform/backend.tf
@@ -102,8 +102,8 @@ resource "aws_security_group" "backend_task" {
 
   egress {
     from_port   = 0
-    to_port     = 65535
-    protocol    = "tcp"
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
